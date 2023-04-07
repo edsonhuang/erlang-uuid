4a23a898fa58e9aa51bb229b0caf9eb740e37911
marshall.huang <marshall.huang@quectel.com>
2023-04-07 17:11:30 +0800

    Change uuid to application add hex support

---
+++ src/uuid_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc uuid public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(uuid_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    uuid_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
